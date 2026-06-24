f6cf2164b1a2542776ef1e370c7b232bfa042b55
codeby-kanishka <kanishka191004@gmail.com>
2026-06-24 10:59:43 +0530

    feat: add exception-safe DynamicArray implementation

--- src/dynamic array/DynamicArray.h
+++ src/dynamic array/DynamicArray.h
@@ -23,16 +23,29 @@ private:
 
         // raw memory allocation
         T* newArr = (T*)malloc(sizeof(T) * newCapacity);
+        if(!newArr) throw std::bad_alloc();
 
         // copy old objects into new memory
-        for(int i = 0; i < currentSize; i++) {
-
+        int i = 0;
+        try{
+        for(; i < currentSize; i++) {
             new(&newArr[i]) T(arr[i]);   // placement new
-
-            arr[i].~T();                 // destroy old object
         }
+    }
+    catch(...) {
+
+    for(int j = 0; j < i; j++)
+        newArr[j].~T();
+
+    free(newArr);
+
+    throw;
+}
 
 
+for(int j = 0; j < currentSize; j++) {
+    arr[j].~T();
+}
         free(arr);
 
         arr = newArr;
@@ -43,7 +56,6 @@ private:
    
     void clear() {
         for(int i = 0; i < currentSize; i++) {
-
             arr[i].~T();
         }
         currentSize = 0;
@@ -59,66 +71,114 @@ public:
         currentCapacity = 4; //initial capacity = 4
 
         arr = (T*)malloc(sizeof(T) * currentCapacity);
+
+        if(!arr)
+    throw std::bad_alloc();
     }
 
    
 DynamicArray(int size, const T& value) {
     currentSize = size;
-    currentCapacity = size;
+      currentCapacity = (size > 4) ? size : 4;
 
     arr = (T*)malloc(sizeof(T)*currentCapacity);
+
+    if(!arr)
+    throw std::bad_alloc();
 
         for(int i = 0; i < currentSize; i++) {
             new(&arr[i]) T(value);
         }
 }
+// COPY CONSTRUCTOR
 
- DynamicArray(const DynamicArray& other) {
+DynamicArray(const DynamicArray& other) {
 
-        currentSize = other.currentSize;
-        currentCapacity = other.currentCapacity;
-
-        arr = (T*)malloc(sizeof(T) * currentCapacity);
-
-        for(int i = 0; i < currentSize; i++) {
-            new(&arr[i]) T(other.arr[i]);
-        }
-    }
-DynamicArray<T>& operator=(const DynamicArray<T>& other) {
-
-    // self assignment check
-    if(this == &other) {
-        return *this;
-    }
-
-
-    // destroy old objects
-    for(int i = 0; i < currentSize; i++) {
-        arr[i].~T();
-    }
-
-    // free old memory
-    free(arr);
-
-
-    // copy new data
     currentSize = other.currentSize;
     currentCapacity = other.currentCapacity;
 
     arr = (T*)malloc(sizeof(T) * currentCapacity);
 
-    for(int i = 0; i < currentSize; i++) {
-        new(&arr[i]) T(other.arr[i]);
+    if(!arr)
+        throw std::bad_alloc();
+
+
+    int i = 0;
+
+    try {
+
+        for(; i < currentSize; i++) {
+            new(&arr[i]) T(other.arr[i]);
+        }
+
     }
+    catch(...) {
+
+        for(int j = 0; j < i; j++) {
+            arr[j].~T();
+        }
+
+        free(arr);
+
+        throw;
+    }
+}
+
+DynamicArray<T>& operator=(const DynamicArray<T>& other) {
+
+    if(this == &other)
+        return *this;
+
+
+    T* newArr = 
+    (T*)malloc(sizeof(T)*other.currentCapacity);
+
+
+    if(!newArr)
+        throw std::bad_alloc();
+
+
+    int i = 0;
+
+    try {
+
+        for(; i < other.currentSize; i++) {
+
+            new(&newArr[i]) T(other.arr[i]);
+        }
+
+    }
+    catch(...) {
+
+        for(int j = 0; j < i; j++)
+            newArr[j].~T();
+
+
+        free(newArr);
+
+        throw;
+    }
+
+    clear();
+    free(arr);
+
+
+    arr = newArr;
+    currentSize = other.currentSize;
+    currentCapacity = other.currentCapacity;
 
 
     return *this;
 }
+
      ~DynamicArray() {
         clear();
         free(arr);
     }
 
+    // METHODS
+
+    //APPEND
     void append(const T& value){
                if(currentSize == currentCapacity) {
                      resize();
@@ -126,28 +186,9 @@ DynamicArray<T>& operator=(const DynamicArray<T>& other) {
           new(&arr[currentSize]) T(value);
         currentSize++;
     }
-
+        
+    //INSERT
     
-        T& get(int index) {
-        if(index < 0 || index >= currentSize)
-            throw std::out_of_range("Invalid index");
-
-        return arr[index];
-    }
-
-        int size() {
-        return currentSize;
-    }
-
-
-    int capacity() {
-        return currentCapacity;
-    }
-
-    bool isEmpty() {
-        return currentSize == 0;
-    }
-
     void insert(int index,const T& value) {
 
         if(index < 0 || index > currentSize)
@@ -165,11 +206,12 @@ DynamicArray<T>& operator=(const DynamicArray<T>& other) {
         currentSize++;
     }
 
-    void remove(int index) {
+    //REMOVE
+
+     void remove(int index) {
 
     if(index < 0 || index >= currentSize)
         throw std::out_of_range("Invalid index");
-
 
     // destroy element to remove
     arr[index].~T();
@@ -177,17 +219,46 @@ DynamicArray<T>& operator=(const DynamicArray<T>& other) {
 
     // shift remaining elements left
     for(int i = index; i < currentSize - 1; i++) {
-
         new(&arr[i]) T(arr[i + 1]);
-
         arr[i + 1].~T();
     }
-
-
     currentSize--;
 }
 
-  
+
+        // get method 
+
+        T& get(int index) {
+
+    if(index < 0 || index >= currentSize)
+        throw std::out_of_range("Invalid index");
+
+    return arr[index];
+}
+
+    //    const T& get(int index) const{
+    //     if(index < 0 || index >= currentSize)
+    //         throw std::out_of_range("Invalid index");
+
+    //     return arr[index];
+    // }
+
+
+    //utility methods
+
+        int size() const{
+        return currentSize;
+    }
+
+
+    int capacity() const{
+        return currentCapacity;
+    }
+
+    bool isEmpty() const {
+        return currentSize == 0;
+    }
+   
   };
   
   #endif
